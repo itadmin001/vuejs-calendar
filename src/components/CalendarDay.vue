ab679c78c0c013f6137ef78d7f1b997a49b0578a
Jeremy Hester <west.fl.itguy@gmail.com>
2020-06-26 04:01:54 -0400

    Fri 06-26-20 0402

---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,33 @@
+<template>
+    <div :class="classObject" @click="captureClick">
+        {{ day.format('D') }}
+        <ul class="event-list">
+            <li v-for="event in events">{{ event.description }}</li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        props: [ 'day' ],
+        computed: {
+            events() {
+                return this.$store.state.events.filter(event => event.date.isSame(this.day, 'day'));
+            },
+            classObject(){
+                let today = this.day.isSame(this.$moment(), 'day')
+                return{
+                    day: true,
+                    today,
+                    past: this.day.isSameOrBefore(this.$moment(), 'day') && !today
+                }
+            }
+        },
+        methods: {
+            captureClick(event) {
+                this.$store.commit('eventFormPos', { x: event.clientX, y: event.clientY });
+                this.$store.commit('eventFormActive', true);
+                this.$store.commit('eventFormDate', this.$moment(this.day, 'YYYY-M-D').format('MMMM DD YYYY'));
+            }
+        }
+    }
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+import moment from 'moment-timezone';
+moment.tz.setDefault('UTC');
+
+export default new Vuex.Store({
+    state: {
+        currentYear: 2020,
+        currentMonth: 6,
+        eventFormPosX: 0,
+        eventFormPosY: 0,
+        eventFormActive: true,
+        events:[
+            { description: 'Random event 1', date: moment('2020-06-25', 'YYYY-MM-DD') },
+            { description: 'Random event 2', date: moment('2020-06-27', 'YYYY-MM-DD') },
+            { description: 'Random event 3', date: moment('2020-07-20', 'YYYY-MM-DD') }
+        ],
+        eventFormDate: null
+    },
+    mutations: {
+        setCurrentMonth(state, payload) {
+            state.currentMonth = payload;
+        },
+        setCurrentYear(state, payload) {
+            state.currentYear = payload;
+        },
+        eventFormPos(state, payload) {
+            state.eventFormPosX = payload.x;
+            state.eventFormPosY = payload.y;
+        },
+        eventFormActive(state, payload) {
+            state.eventFormActive = payload;
+        },
+        addEvent(state, payload) {
+            state.events.push({
+                description: payload,
+                date: state.eventFormDate
+            });
+        },
+        eventFormDate(state, payload) {
+            state.eventFormDate = payload
+        }
+    }
+});
